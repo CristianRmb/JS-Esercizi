87ef34efce492ed7604538275edc5fadc717bd1a
Cristian <cristianrimbu@gmail.com>
2022-05-13 12:29:10 +0200

    es svolto

--- es13/exercise-3.js
+++ es13/exercise-3.js
@@ -1,19 +1,25 @@
-const sum = function (a, b) {
-  return a + b;
+const sum = (a, b) => {
+  // return a + b;
+  return `(${a} + ${b})`
 }
 
-const subtract = function (a, b) {
-  return a - b;
+const subtract = (a, b) => {
+  // return a - b;
+  return `(${a} - ${b})`;
 }
 
-const multiply = function (a, b) {
-  return a * b;
+const multiply =  (a, b) => {
+  // return a * b;
+  return `${a} * ${b}`;
 }
 
-const divide = function (a, b) {
-  return a / b;
+const divide =  (a, b) => {
+  //return a / b;
+  return `${a} / ${b}`;
 }
-
-const log = function (value) {
-  console.log(value);
+/* ho modificato i return per far vedere l'operazione matematica e non il risultato */
+const log = () => {
+  let operazMat = divide( subtract( multiply( sum(2, 4), sum(5, 2) ), 2), 5);
+  console.log(operazMat);
 }
+log();
