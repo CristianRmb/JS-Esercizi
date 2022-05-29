495801e712a4a777a6436071a3633e4b710d6632
Cristian <cristianrimbu@gmail.com>
2022-05-29 16:32:31 +0200

    es corretto

--- es27/exercise-1.js
+++ es27/exercise-1.js
@@ -1,19 +1,24 @@
 const person = {
   firstName: 'John',
   lastName: 'Doe',
-  // fullName: function () {
-  //   return console.log("la funzione esiste");
-  // }
 };
 
-if (person.address) {
+// if (person.address) {
+//   console.log(person.address.city); // Argh! Cannot read property 'city' of undefined
+// } else {
+//   console.log("proprieta person.address.city non esiste");
+// }
+
+// if (typeof person.fullName === "function"){
+//   console.log(person.fullName()); // Argh! Cannot read property 'fullName()' of undefined
+// } else {
+//   console.log("la funzione fullName non esiste");
+// }
+
+if (person?.address?.city){
   console.log(person.address.city); // Argh! Cannot read property 'city' of undefined
-} else {
-  console.log("proprieta person.address.city non esiste");
 }
 
-if (typeof person.fullName === "function"){
+if (person?.fullName?.()){
   console.log(person.fullName()); // Argh! Cannot read property 'fullName()' of undefined
-} else {
-  console.log("la funzione fullName non esiste");
 }
